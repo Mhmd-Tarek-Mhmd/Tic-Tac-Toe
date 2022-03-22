54f39d7fa8a4fba30572bda19ab68a003771f096
Mhmd <biboo.biboo97@gmail.com>
2022-03-22 19:54:20 +0200

    Add service worker

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = "tic-tac-toc-pwa";
+const filesToCache = [
+  "index.html",
+  "manifest.json",
+  "icons/icon-180.png",
+  "icons/icon-192.png",
+  "icons/icon-512.png",
+  "scripts/game.js",
+  "scripts/custom-elements-polyfill.min.js",
+];
+
+self.addEventListener("install", (e) =>
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  )
+);
+
+self.addEventListener("fetch", (e) => e.respondWith(caches.match(e.request)));
